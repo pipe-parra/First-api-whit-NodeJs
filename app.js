1d3e6afbbd69402c9e680bfd400604c590fa5df3
pipe <felipe.parra@blackmind.cl>
2022-07-24 02:38:24 -0400

    configuramos app.js cpn todos los requerimientos, ahora debemos instalar dependencias , obviamente arrojara error porque no hemos creado la base de datos

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3050;
+
+const app = express();
+
+app.use(bodyParser().json());
+
+//mysql
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'rootpass',
+    database:'node20_mysql'    
+});
+
+
+//check Connect
+connection.connect(error=>{
+    if(error) throw error;
+    console.log('DataBase server running!');
+});
+
+app.listen(PORT, ()=>{console.log(`SERVER RUNNING ON PORT ${PORT}`);})
